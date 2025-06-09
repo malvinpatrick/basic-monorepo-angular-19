9f7c346341d423c922bc0b78dfc7696ca4c2fe62
Malvin Patrick Kurniawan <malvin.p.kurniawan@devbz.local>
2025-06-09 14:00:54 +0700

    Implement logging framework with color support and configurable loggers

--- projects/example-project/src/app/app.component.ts
+++ projects/example-project/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { PersonCardComponent, TeamDisplayComponent } from 'example-library';
+import { provideLogger } from '../shared/logger/providers';
+import { defaultConfig, LoggerConfig } from '../shared/logger/logger-config';
+import { LoggerService } from '../shared/logger/logger';
+
+// const tmp: LoggerConfig = 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [PersonCardComponent, TeamDisplayComponent],
+  providers: [
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    logger = inject(LoggerService)
   title = 'example-project';
+
+    ngOnInit(): void {
+        this.logger.info('AppComponent', 'AppComponent initialized');
+        this.logger.debug('AppComponent', 'Debugging AppComponent');
+        this.logger.error('AppComponent', 'An error occurred in AppComponent');
+    }
 }
--- projects/example-project/src/app/app.config.ts
+++ projects/example-project/src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideLogger } from '../shared/logger/providers';
+import { LogLevel } from '../shared/logger/log-level';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideLogger({
+      level: LogLevel.DEBUG,
+    }),
+  ],
 };
